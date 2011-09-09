893d52b9b128b72bf5b5a6ae9a91f9628630e79b
Simone Leo <simone.leo@crs4.it>
2011-09-09 09:38:55 +0000

    core: fixed problem with double instance of nib.py module

--- bl/core/seq/utils/nib.py
+++ bl/core/seq/utils/nib.py
@@ -1,21 +1,31 @@
-## .nib format 	
-  	
+"""
+Support for the .nib format::
 
-## The .nib format pre-dates the .2bit format and is less compact. It describes a DNA sequence by packing two bases into each byte. Each .nib file contains only a single sequence. The file begins with a 32-bit signature that is 0x6BE93D3A in the architecture of the machine that created the file (or possibly a byte-swapped version of the same number on another machine). This is followed by a 32-bit number in the same format that describes the number of bases in the file. Next, the bases themselves are listed, packed two bases to the byte. The first base is packed in the high-order 4 bits (nibble); the second base is packed in the low-order four bits:
+  http://genome.ucsc.edu/FAQ/FAQformat.html#format8
 
-##     byte = (base1<<4) + base2 
+The .nib format pre-dates the .2bit format and is less compact. It
+describes a DNA sequence by packing two bases into each byte. Each
+.nib file contains only a single sequence. The file begins with a
+32-bit signature that is 0x6BE93D3A in the architecture of the machine
+that created the file (or possibly a byte-swapped version of the same
+number on another machine). This is followed by a 32-bit number in the
+same format that describes the number of bases in the file. Next, the
+bases themselves are listed, packed two bases to the byte. The first
+base is packed in the high-order 4 bits (nibble); the second base is
+packed in the low-order four bits:
 
-## The numerical representations for the bases are:
+    byte = (base1<<4) + base2 
 
-##     * 0 - T
-##     * 1 - C
-##     * 2 - A
-##     * 3 - G
-##     * 4 - N (unknown) 
+The numerical representations for the bases are:
 
-## The most significant bit in a nibble is set if the base is masked.
+    * 0 - T
+    * 1 - C
+    * 2 - A
+    * 3 - G
+    * 4 - N (unknown) 
 
-## http://genome.ucsc.edu/FAQ/FAQformat.html#format8
+The most significant bit in a nibble is set if the base is masked.
+"""
 
 import struct
 from ctypes import create_string_buffer
@@ -43,33 +53,5 @@ def seq2nib(seq):
         value |= 8
     except IndexError:
       pass
-    try:
-      struct.pack_into('@B', nib, offset, value)
-    except struct.error:
-      print i, seq[i-10:i+10]
-      raise
+    struct.pack_into('@B', nib, offset, value)
   return nib.raw
-
-
-def nib2seq(nib):
-  sig = struct.unpack_from('@i', nib, 0)
-  if sig != SIGNATURE and sig != BYTE_SWAPPED_SIGNATURE:
-    raise ValueError("bad signature: 0x%x" % sig)
-  seq_len = struct.unpack_from('@i', nib, 4)
-  
-  
-
-if __name__ == "__main__":
-  chrom = "22"
-  from bl.core.seq.io.fasta import RawFastaReader
-  with open("chr%s.fa" % chrom) as f:
-    reader = RawFastaReader(f)
-    records = [r for r in reader]
-  assert len(records) == 1
-  header, seq = records[0]
-  nib = seq2nib(seq)
-  ## with open("try_chrM.nib", 'w') as outf:
-  ##   outf.write(nib)
-  with open("chr%s.nib" % chrom) as f:
-    exp_nib = f.read()
-  print "nib == exp_nib:", (nib == exp_nib)
