0d406470d522875265684aec9634fa257ea16dd2
Gianluigi Zanetti <gianluigi.zanetti@crs4.it>
2011-10-27 19:57:16 +0000

    oops 2

---
+++ bl/core/gt/messages/SnpCall.py
@@ -0,0 +1,10 @@
+from details.SampleSnpCall_pb2 import sc_A as A
+from details.SampleSnpCall_pb2 import sc_B as B
+from details.SampleSnpCall_pb2 import sc_AA as AA
+from details.SampleSnpCall_pb2 import sc_AB as AB
+from details.SampleSnpCall_pb2 import sc_BB as BB
+from details.SampleSnpCall_pb2 import sc_NOCALL as NOCALL
+from details.SampleSnpCall_pb2 import sc_MAXCALLCODE as MAXCALLCODE
+
+NC = NOCALL
+MC = MAXCALLCODE
