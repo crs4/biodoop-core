6ffff88f0259cd2688d2aa6e58698f44ac7bf005
Simone Leo <simone.leo@crs4.it>
2012-01-11 17:23:03 +0000

    core: fixed a test

--- test/seq/stats/try_karlin_altschul.py
+++ test/seq/stats/try_karlin_altschul.py
@@ -1,6 +1,7 @@
 # BEGIN_COPYRIGHT
 # END_COPYRIGHT
-import sys, logging
+
+import sys, os, logging
 from subprocess import Popen, PIPE
 from bl.core.seq.stats.karlin_altschul import BlastallLKCalculator
 
@@ -8,26 +9,18 @@ from bl.core.seq.stats.karlin_altschul import BlastallLKCalculator
 ENGINE_OPTS = {"blastall.program": "blastn"}
 
 
-formatdb_exe, err = Popen("which formatdb", shell=True,
-                          stdout=PIPE, stderr=PIPE).communicate()
-if err:
-  sys.exit("formatdb executable not found")
-formatdb_exe = formatdb_exe.strip()
-blastall_exe, err = Popen("which blastall", shell=True,
-                          stdout=PIPE, stderr=PIPE).communicate()
-if err:
-  sys.exit("blastall executable not found")
-blastall_exe = blastall_exe.strip()
-
-
-calculator = BlastallLKCalculator(
-  formatdb_exe,
-  blastall_exe,
-  log_level=logging.DEBUG,
-  engine_opts=ENGINE_OPTS,
-  )
-
-lambda_, k = calculator.calculate()
-
-print "engine options: %r" % ENGINE_OPTS
-print "lambda, k = %f, %f" % (lambda_, k)
+if __name__ == "__main__":
+  formatdb_exe = os.getenv("BL_FORMATDB_EXE", "/usr/bin/formatdb").strip()
+  blastall_exe = os.getenv("BL_BLASTALL_EXE", "/usr/bin/blastall").strip()
+  for exe in formatdb_exe, blastall_exe:
+    if not (os.path.isfile(exe) and os.access(exe, os.X_OK)):
+      sys.exit("%s not found or not executable" % exe)
+  calculator = BlastallLKCalculator(
+    formatdb_exe,
+    blastall_exe,
+    log_level=logging.DEBUG,
+    engine_opts=ENGINE_OPTS,
+    )
+  lambda_, k = calculator.calculate()
+  print "engine options: %r" % ENGINE_OPTS
+  print "lambda, k = %f, %f" % (lambda_, k)
