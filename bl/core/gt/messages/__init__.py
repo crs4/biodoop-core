d9a223889fe695743dcad5b078f89f2355f8f095
Simone Leo <simone.leo@crs4.it>
2013-03-21 17:21:50 +0000

    gt/messages: fixed module name

--- bl/core/gt/messages/__init__.py
+++ bl/core/gt/messages/__init__.py
@@ -8,4 +8,4 @@ Message conversion utilities.
 import bl.core.messages.registry
 
 # the following will automagically register the relevant message codecs
-from SampleSnpCall import SampleSnpCall
+from sample_snp_call import SampleSnpCall
