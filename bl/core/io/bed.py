a91e668955e7a18b99df6898fdac948bad0db7c9
Simone Leo <simone.leo@crs4.it>
2013-05-29 10:51:09 +0000

    bl/core/io/bed.py: added read functionality

--- bl/core/io/bed.py
+++ bl/core/io/bed.py
@@ -1,32 +1,55 @@
 """
 `BED <https://genome.ucsc.edu/FAQ/FAQformat.html#format1>`_ I/O.
 """
-import csv
+
+import __builtin__
+import os, csv
+
+
+def cs_int(s):
+    return map(int, s.strip(",").split(","))
 
 REQUIRED_FIELDS = [
-    "chrom",       # name of the chromosome or scaffold, e.g., 'chr1'
-    "chromStart",  # 0-based index
-    "chromEnd",    # 0-based index plus 1, as in range()
+    ("chrom", str),          # name of the chromosome or scaffold, e.g., 'chr1'
+    ("chromStart", int),     # 0-based index
+    ("chromEnd", int),       # 0-based index plus 1, as in range()
     ]
 OPTIONAL_FIELDS = [
-    "name",        # name of the feature
-    "score",       # 0-1000, higher numbers = darker gray when visualized
-    "strand",      # '+' or '-'
-    "thickStart",  # starting position at which the feature is drawn thickly
-    "thickEnd",    # ending position at which the feature is drawn thickly
-    "itemRgb",     # 0-255,0-255,0-255 display color
-    "blockCount",  # number of blocks (exons)
-    "blockSizes",  # comma-separated block sizes
-    "blockStarts", # comma-separated block starts, relative to chromStart
+    ("name", str),           # name of the feature
+    ("score", int),          # 0-1000, determines gray level when visualized
+    ("strand", str),         # '+' or '-'
+    ("thickStart", int),     # draw feature thickly starting from this position
+    ("thickEnd", int),       # stop drawing feature thickly at this position
+    ("itemRgb", cs_int),     # 0-255,0-255,0-255 display color
+    ("blockCount", int),     # number of blocks (exons)
+    ("blockSizes", cs_int),  # block sizes
+    ("blockStarts", cs_int), # block starts, relative to chromStart
     ]
 FIELDS = REQUIRED_FIELDS + OPTIONAL_FIELDS
+FIELDNAMES = [f[0] for f in FIELDS]
 
 
-class BedReader(object):
+class BedFile(object):
 
-    def __init__(self, f):
-        self.f = f
-        self.csv_reader = csv.DictReader(self.f, FIELDS, delimiter="\t")
+    PARAMS = {"delimiter": "\t", "lineterminator": os.linesep}
+
+    def __init__(self, name, mode="r", buffering=-1):
+        if mode == "r":
+            wrapper_type = csv.DictReader
+        elif mode == "w" or mode == "a":
+            wrapper_type = csv.DictWriter
+        else:
+            raise ValueError("opening mode '%s' not supported" % mode)
+        self.f = __builtin__.open(name, mode, buffering)
+        self.__wrapper = wrapper_type(self.f, FIELDNAMES, **self.PARAMS)
+
+    @property
+    def name(self):
+        return self.f.name
+
+    @property
+    def closed(self):
+        return self.f.closed
 
     def close(self):
         self.f.close()
@@ -38,6 +61,14 @@ class BedReader(object):
         self.close()
 
     def next(self):
-        r = self.csv_reader.next()
-        # TODO: type conversion
+        r = self.__wrapper.next()
+        for fname, ftype in FIELDS:
+            r[fname] = ftype(r[fname])
         return r
+
+    def __iter__(self):
+        return self
+
+
+def open(name, mode="r", buffering=-1):
+    return BedFile(name, mode, buffering)
