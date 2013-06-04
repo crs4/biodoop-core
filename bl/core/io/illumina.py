7a39ba1052cce6d48652d656796228ef0490852b
Simone Leo <simone.leo@crs4.it>
2013-06-04 15:15:02 +0000

    added copyright notice to all relevant files

--- bl/core/io/illumina.py
+++ bl/core/io/illumina.py
@@ -1,4 +1,22 @@
 # BEGIN_COPYRIGHT
+# 
+# Copyright (C) 2009-2013 CRS4.
+# 
+# This file is part of biodoop-core.
+# 
+# biodoop-core is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+# 
+# biodoop-core is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+# 
+# You should have received a copy of the GNU General Public License along with
+# biodoop-core.  If not, see <http://www.gnu.org/licenses/>.
+# 
 # END_COPYRIGHT
 
 import csv, re
